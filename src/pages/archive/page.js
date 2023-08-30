68bd65817b85d6c28a48a444edf98cfa052fdcba
Matteo Savoia <matteo.savoia@applied.it>
2023-08-30 10:07:39 +0200

    struttura componente card

--- src/pages/archive/page.js
+++ src/pages/archive/page.js
@@ -1,5 +1,23 @@
+import ErrorCacher from "../../components/Errors/ErrorCacher";
+import {
+    Grid,
+    Box,
+    Card,
+    CardContent,
+    CardActions,
+    CardMedia,
+    Container,
+    Typography,
+  } from "@mui/material";
 export function Archive() {
     return (
-        <h1>Archive</h1>
-    )
-}
+        
+        <ErrorCacher>
+          <Container sx={{ flexGrow: 1 }} disableGutters>
+            <Card sx={{ mt: 1 }}>
+            <h1>Archive</h1>
+            </Card>
+          </Container>
+        </ErrorCacher>
+      );
+    }
