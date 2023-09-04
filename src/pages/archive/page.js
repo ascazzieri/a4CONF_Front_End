ba8a380dffaf9b31d8fce154a0f94ab4f298b609
Matteo Savoia <matteo.savoia@applied.it>
2023-09-04 16:58:08 +0200

    bottone modify implementato

--- src/pages/archive/page.js
+++ src/pages/archive/page.js
@@ -45,7 +45,8 @@ export function Archive() {
     setArchive(newArchive);
   };
   const handleModify = (item) => {
-    
+    setTitle(item);
+    setContent(archive[item]);
   };
   return (
     <ErrorCacher>
